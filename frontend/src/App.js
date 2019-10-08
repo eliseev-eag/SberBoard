20fe95b3ef304ac4cb38f1486ae0b4d2fdd24979
Eliseev Aleksei <eliseev-eag@yandex.ru>
2019-10-08 16:40:11 +0700

    Добавил Toolbar mock и Floating Action Button mock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Toolbar, IconButton, Fab, Button, makeStyles } from '@material-ui/core';
+import { Add, Menu } from '@material-ui/icons';
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 0,
+    flexShrink: 0,
+    width: '100vw',
+    height: '100vh',
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(5),
+    right: theme.spacing(5),
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <Menu />
+          </IconButton>
+          <div className={classes.grow} />
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Fab color="primary" aria-label="add" className={classes.fab}>
+        <Add />
+      </Fab>
     </div>
   );
-}
+};
 
 export default App;
