2b727c1a983a3a23929ce4949fd1c8f86d64452c
Eliseev Alexey <aeliseevwork@gmail.com>
2019-10-16 21:20:06 +0700

    Добавил пример карточки для LineChart

--- frontend/src/pages/AddChartPage.js
+++ frontend/src/pages/AddChartPage.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import { Grid, makeStyles, Paper, Typography } from '@material-ui/core';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 const useStyles = makeStyles(theme => ({
-  header: {
+  chartPaper: {
     padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
   },
 }));
 
@@ -14,12 +57,33 @@ const AddChartPage = () => {
   const styles = useStyles();
 
   return (
-    <Grid item xs={12}>
-      <Paper className={styles.header}>
-        <Typography color="primary" gutterBottom>
-          Создание графика
-        </Typography>
-      </Paper>
+    <Grid container xs={12}>
+      <Grid item xs={12} md={6}>
+        <Paper className={styles.chartPaper}>
+          <Typography align="center" color="textPrimary" gutterBottom component="h5">
+            Line chart
+          </Typography>
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </Paper>
+      </Grid>
     </Grid>
   );
 };
