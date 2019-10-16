77c8a9a6d7f252aeafcfc12da88a97ccc4fe613d
Eliseev Alexey <aeliseevwork@gmail.com>
2019-10-16 22:27:28 +0700

    Добавил примеры Area и Bar chart-ов

--- frontend/src/pages/AddChartPage/ChartDemos/demosMap.js
+++ frontend/src/pages/AddChartPage/ChartDemos/demosMap.js
@@ -1,3 +1,9 @@
 import LinearChartDemo from './LinearChartDemo';
+import BarChartDemo from './BarChartDemo';
+import AreaChartDemo from './AreaChartDemo';
 
-export default [{ title: 'Line chart', component: LinearChartDemo }];
+export default [
+  { title: 'Line chart', component: LinearChartDemo },
+  { title: 'Area chart', component: AreaChartDemo },
+  { title: 'Bar chart', component: BarChartDemo },
+];
