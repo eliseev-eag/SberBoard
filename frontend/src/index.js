159871396eeb906326285f3c3430f3a57d8923e3
Eliseev Aleksei <eliseev-eag@yandex.ru>
2019-10-22 12:18:12 +0700

    Добавил ThemeProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import App from './App';
 import 'typeface-roboto';
 
+const theme = createMuiTheme({
+  palette: { background: { default: '#f0f0f0' } },
+});
+
 const AppWrapper = () => (
   <HashRouter>
-    <CssBaseline />
-    <App />
+    <ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ThemeProvider>
   </HashRouter>
 );
 
