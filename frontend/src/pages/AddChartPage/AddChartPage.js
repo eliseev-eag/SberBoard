ebd7d7812d3560ebba3211ea890eb94090973ab5
Eliseev Alexey <eliseev-eag@yandex.ru>
2019-10-24 15:12:45 +0700

    Добавил React-json-viewer

--- frontend/src/pages/AddChartPage/AddChartPage.js
+++ frontend/src/pages/AddChartPage/AddChartPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Grid, makeStyles, Paper, Typography } from '@material-ui/core';
+import ReactJson from 'react-json-view';
 import demosMap from './ChartDemos';
 
 const useStyles = makeStyles(theme => ({
   chartPaper: {
     padding: theme.spacing(2),
-    margin: theme.spacing(2),
+    margin: theme.spacing(0, 2, 2, 2),
   },
   chartHeader: {
     color: theme.palette.primary.main,
@@ -17,7 +18,25 @@ const useStyles = makeStyles(theme => ({
   container: {
     paddingBottom: theme.spacing(3),
   },
+  viewerWrapper: {
+    fontSize: 18,
+  },
 }));
+
+const exampleJson = {
+  string: 'this is a test string',
+  integer: 42,
+  array: [1, 2, 3, 'test', NaN],
+  float: 3.14159,
+  undefined: undefined,
+  object: {
+    'first-child': true,
+    'second-child': false,
+    'last-child': null,
+  },
+  string_number: '1234',
+  date: new Date(),
+};
 
 const AddChartPage = () => {
   const classNames = useStyles();
@@ -36,7 +55,7 @@ const AddChartPage = () => {
           Выберите тип графика для продолжения
         </Typography>
       </Grid>
-      <Grid item container xs={12} md={12} direction="row" alignItems="center" justify="space-around">
+      <Grid item container xs={12} md={12} direction="row" alignItems="flex-start" justify="space-around">
         <Grid item container xs={6} direction="column" justify="space-around" alignItems="stretch">
           {demosMap.map(({ title, component: Component }) => (
             <Grid item xs={12} md={12} key={title}>
@@ -55,10 +74,16 @@ const AddChartPage = () => {
             </Grid>
           ))}
         </Grid>
-        <Grid item container xs={6} direction="column" justify="space-around" alignItems="center">
-          <Typography color="primary" align="center" component="h4" className={classNames.title}>
-            Выберите тип графика для продолжения
-          </Typography>
+        <Grid
+          item
+          container
+          xs={6}
+          direction="column"
+          justify="flex-start"
+          alignItems="center"
+          className={classNames.viewerWrapper}
+        >
+          <ReactJson src={exampleJson} enableClipboard={false} collapsed={3} />
         </Grid>
       </Grid>
     </Grid>
