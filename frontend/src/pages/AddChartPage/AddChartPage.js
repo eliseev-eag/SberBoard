b7f3a33d7663d53b89ec23a95ac6fa08acca50ea
Eliseev Alexey <eliseev-eag@yandex.ru>
2019-10-24 14:42:18 +0700

    поправил layout приложения

--- frontend/src/pages/AddChartPage/AddChartPage.js
+++ frontend/src/pages/AddChartPage/AddChartPage.js
@@ -5,6 +5,7 @@ import demosMap from './ChartDemos';
 const useStyles = makeStyles(theme => ({
   chartPaper: {
     padding: theme.spacing(2),
+    margin: theme.spacing(2),
   },
   chartHeader: {
     color: theme.palette.primary.main,
@@ -35,22 +36,31 @@ const AddChartPage = () => {
           Выберите тип графика для продолжения
         </Typography>
       </Grid>
-      {demosMap.map(({ title, component: Component }) => (
-        <Grid item xs={12} md={6} key={title}>
-          <Paper className={classNames.chartPaper}>
-            <Typography
-              align="center"
-              color="textPrimary"
-              gutterBottom
-              component="h5"
-              className={classNames.chartHeader}
-            >
-              {title}
-            </Typography>
-            <Component />
-          </Paper>
+      <Grid item container xs={12} md={12} direction="row" alignItems="center" justify="space-around">
+        <Grid item container xs={6} direction="column" justify="space-around" alignItems="stretch">
+          {demosMap.map(({ title, component: Component }) => (
+            <Grid item xs={12} md={12} key={title}>
+              <Paper className={classNames.chartPaper}>
+                <Typography
+                  align="center"
+                  color="textPrimary"
+                  gutterBottom
+                  component="h5"
+                  className={classNames.chartHeader}
+                >
+                  {title}
+                </Typography>
+                <Component />
+              </Paper>
+            </Grid>
+          ))}
         </Grid>
-      ))}
+        <Grid item container xs={6} direction="column" justify="space-around" alignItems="center">
+          <Typography color="primary" align="center" component="h4" className={classNames.title}>
+            Выберите тип графика для продолжения
+          </Typography>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
