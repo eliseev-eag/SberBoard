183dfeb23fa776911bbf7cdaf32003afa3f85e36
Eliseev Alexey <eliseev-eag@yandex.ru>
2019-10-25 04:28:49 +0700

    Поправил стили на списке целей

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import { Fab, Grid, makeStyles, Paper } from '@material-ui/core';
-import { Add } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
+import { Typography, Grid, makeStyles, Paper } from '@material-ui/core';
+import { AddBoxOutlined } from '@material-ui/icons';
 import { addChartRoute } from '../routes';
 
 const useStyles = makeStyles(theme => ({
@@ -10,12 +10,32 @@ const useStyles = makeStyles(theme => ({
     bottom: theme.spacing(5),
     right: theme.spacing(5),
   },
-  paper: {
+  goal: {
+    width: 300,
+    height: 300,
     padding: theme.spacing(2),
+    margin: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  goalHeader: {
+    fontSize: 28,
+  },
+  addGoalButton: {
+    border: '2px dashed #e0e0e0',
+    cursor: 'pointer',
+  },
+  addGoalButtonIcon: {
+    color: '#e0e0e0',
+    fontSize: theme.typography.fontSize * 5,
   },
 }));
 
 const MainPage = () => {
+  const history = useHistory();
   const classes = useStyles();
 
   const [goals, setGoals] = useState([]);
@@ -27,18 +47,18 @@ const MainPage = () => {
   }, []);
 
   return (
-    <>
-      <Fab color="primary" aria-label="add" className={classes.fab} component={RouterLink} to={addChartRoute}>
-        <Add />
-      </Fab>
-      <Grid item container xs={12} justify="space-between" alignItems="center">
-        {goals.map(it => (
-          <Grid item xs={4}>
-            <Paper className={classes.paper}>{it.name}</Paper>
-          </Grid>
-        ))}
-      </Grid>
-    </>
+    <Grid item container xs={12} justify="flex-start" alignItems="flex-start">
+      {goals.map(it => (
+        <Paper className={classes.goal} key={it.name}>
+          <Typography component="h5" color="primary" className={classes.goalHeader}>
+            {it.name}
+          </Typography>
+        </Paper>
+      ))}
+      <Paper className={`${classes.goal} ${classes.addGoalButton}`} onClick={() => history.push(addChartRoute)}>
+        <AddBoxOutlined className={classes.addGoalButtonIcon} />
+      </Paper>
+    </Grid>
   );
 };
 
